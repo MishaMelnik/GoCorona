76b2b050870704f3333eaf035db33c915cb98a5b
misha <melnik_mo@groupbwt.com>
2022-02-20 21:49:02 +0200

    feat: created header and 2 elements main

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/style.scss'
+
+//burger menu
+const iconMenu = document.querySelector('.header__burger');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
