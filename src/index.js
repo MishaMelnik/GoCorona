267c01afe7029fc850e1310e22a980044037e114
misha <melnik_mo@groupbwt.com>
2022-02-21 12:40:41 +0200

    feat: finish version project

--- src/index.js
+++ src/index.js
@@ -10,3 +10,31 @@ if (iconMenu) {
         menuBody.classList.toggle('_active');
     })
 }
+// scroll on click
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            })
+            e.preventDefault();
+        }
+    }
+}
